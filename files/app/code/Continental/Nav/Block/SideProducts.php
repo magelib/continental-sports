b545537b4feed8570c2b26ef8bc338e7277999cf
Matthew Byfield <matthew.byfield@attercopia.co.uk>
2017-07-18 11:37:26 +0000

    Background image for categories

--- files/app/code/Continental/Nav/Block/SideProducts.php
+++ files/app/code/Continental/Nav/Block/SideProducts.php
@@ -19,6 +19,8 @@ class SideProducts
     /** @var Context $httpContext */
     protected $httpContext;
 
+/** @var object $category */
+    protected $category;
     /**
      * Links constructor.
      * @param Template\Context $context
@@ -30,14 +32,21 @@ class SideProducts
     {
         $this->_customerUrl = $customerUrl;
         $this->httpContext = $httpContext;
+	$this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+	$this->category = $this->objectManager->get('Magento\Framework\Registry')->registry('current_category');
+	
 
         parent::__construct($context, $data);
     }
 
+    public function categoryBackgroundImage() {
+        $imageHtml =  $this->category->getImageUrl();
+	return $imageHtml;
+	}
+
 	function SideSubMenu($showImages = false) {
-		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-		$category = $objectManager->get('Magento\Framework\Registry')->registry('current_category');//get current category
-		$subcats = $category->getChildrenCategories();
+		$subcats = $this->category->getChildrenCategories();
 //		$_helper = $this->helper('Magento\Catalog\Helper\Output');
 ?>
 
@@ -45,7 +54,7 @@ class SideProducts
     <?php
     foreach ($subcats as $subcat) {
         if ($subcat->getIsActive()) {
-            $_category = $objectManager->create('Magento\Catalog\Model\Category')->load($subcat->getId());
+            $_category = $this->objectManager->create('Magento\Catalog\Model\Category')->load($subcat->getId());
             $subcaturl = $subcat->getUrl();
 
                 /* @escapeNotVerified */
