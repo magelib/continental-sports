5926a131e1c90eaa5cd57613d0f85023986de4d2
Matthew Byfield <matthew.byfield@attercopia.co.uk>
2017-08-02 11:55:27 +0000

    Testing Sku changer module

---
+++ files/app/code/Continental/Products/Block/Details.php
@@ -0,0 +1,15 @@
+<?php
+namespace Continental\Products\Block;
+
+/**
+*  Details block
+*/
+
+
+class Details extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+}
---
+++ files/app/code/Continental/Products/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_ConfigurableProduct/js/configurable': {
+                'YourVendor_YourModule/js/model/skuswitch': true
+            }
+        }
+    }
+};
