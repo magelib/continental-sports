0540774cad2e84379730cefe4e9801c410048aab
Matthew Byfield <matthew.byfield@attercopia.co.uk>
2017-08-10 16:33:51 +0000

    Partial fix to Banners

--- files/app/code/Continental/Banners/Block/Homepage.php
+++ files/app/code/Continental/Banners/Block/Homepage.php
@@ -4,28 +4,36 @@ namespace Continental\Banners\Block;
 class Homepage extends \Magento\Framework\View\Element\Template
 {
 
-    /**
-     * @var \Amasty\HelloWorld\Helper\Data
-     */
     protected $_helper;
 
     /**
      * @var \Magento\Framework\ObjectManagerInterface
      */
     protected $_objectManager;
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $_scopeConfig;
 
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context, array $data = [],
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = [],
         \Continental\Banners\Helper\Data $helper,
-        \Magento\Framework\ObjectManagerInterface $objectManager
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
     ) {
         parent::__construct($context, $data);
         $this->_helper = $helper;
         $this->_objectManager = $objectManager;
+        $this->_scopeConfig = $scopeConfig;
     }
 
     public function getBlockTitle() {
-	return 'Jumpy' . $this->_helper->getBlockTitle();
+     /* Note to self; fix the Continental_Banners case in the helper files and this should all work */
+        //return 'Jumpy' . $this->_scopeConfig->getBlockTitle();
+        //return $this->_helper->getBlockTitle();
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        return $this->_scopeConfig->getValue('Continental_Banners/general/block_text', $storeScope);
     }
 
     public function getBlockLabel(){
