6002911f0f0af266a30fa93713f733ed6c057877
Matthew Byfield <matthew.byfield@attercopia.co.uk>
2017-08-30 14:27:57 +0000

    Updated merge

--- app/code/Continental/HomeBanners/Controller/Adminhtml/Template/Edit.php
+++ app/code/Continental/HomeBanners/Controller/Adminhtml/Template/Edit.php
@@ -32,38 +32,38 @@ class Edit extends \Continental\HomeBanners\Controller\Adminhtml\Template
      */
     public function execute()
     {
-/*
         $model = $this->_objectManager->create('Continental\HomeBanners\Model\Template');
         $id = $this->getRequest()->getParam('id');
         if ($id) {
             $model->load($id);
         }
 
-        $this->_coreRegistry->register('_edit_template', $model);
+        $this->_coreRegistry->register('_homebanners_template', $model);
 
         $this->_view->loadLayout();
-        $this->_setActiveMenu('Continental_HomeBanners::edit_template');
+        $this->_setActiveMenu('Continental_HomeBanners::homebanners_template');
 
         if ($model->getId()) {
-            $breadcrumbTitle = __('Edit Template');
+            $breadcrumbTitle = __('Edit Banner');
             $breadcrumbLabel = $breadcrumbTitle;
         } else {
-            $breadcrumbTitle = __('New Template');
-            $breadcrumbLabel = __('Create  Template');
+            $breadcrumbTitle = __('New Banner');
+            $breadcrumbLabel = __('Create  Banner');
         }
-        $this->_view->getPage()->getConfig()->getTitle()->prepend(__(' Templates'));
+        $this->_view->getPage()->getConfig()->getTitle()->prepend(__(' Banners'));
         $this->_view->getPage()->getConfig()->getTitle()->prepend(
-            $model->getId() ? $model->getTemplateId() : __('New Template')
+            $model->getId() ? $model->getTemplateId() : __('New Banner')
         );
 
         $this->_addBreadcrumb($breadcrumbLabel, $breadcrumbTitle);
 
         // restore data
-        $values = $this->_getSession()->getData('edit_template_form_data', true);
+        $values = $this->_getSession()->getData('homebanner_template_form_data', true);
         if ($values) {
             $model->addData($values);
         }
-*/
+
         $this->_view->renderLayout();
     }
 }
+
