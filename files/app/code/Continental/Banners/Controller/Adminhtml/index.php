56ca6d13f1d8669b5869643604d6c11aa4f30b6c
Jamie Youngman <MY_NAME@example.com>
2017-09-07 10:16:03 +0000

    backup env file

--- files/app/code/Continental/Banners/Controller/Adminhtml/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Continental\Banners\Controller\Adminhtml;
-class Index extends \Magento\Framework\App\Action\Action
-{
-    public function execute()
-    {
-        die('test index');
-    }
-}
