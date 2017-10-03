bbdc433ec60605c21c1aea968122b86984c17a9f
Matthew Byfield <matthew.byfield@attercopia.co.uk>
2017-10-03 15:11:11 +0000

    Fix and frontend tidy of swatch and configurable selection

--- app/code/Continental/Products/view/frontend/requirejs-config.js
+++ app/code/Continental/Products/view/frontend/requirejs-config.js
@@ -3,6 +3,9 @@ var config = {
         mixins: {
             'Magento_ConfigurableProduct/js/configurable': {
                 'Continental_Products/js/model/skuswitch': true
+            },
+            'Magento_Swatches/js/swatch-renderer': {
+                'Continental_Products/js/model/swatch-skuswitch': true
             }
         }
     }
