e263bd4758390106cdc99b15166939e86b326d3a
Jamie Youngman <MY_NAME@example.com>
2017-11-13 10:04:01 +0000

    Adding latest wardsoft changes

---
+++ app/code/Continental/Banners/Controller/Adminhtml/index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Continental\Banners\Controller\Adminhtml;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        die('test index');
+    }
+}
